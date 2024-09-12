2b247afaf8a783ba2e53090e5bef9cf2a798ba89
Christian Jaeger <ch@christianjaeger.ch>
2024-09-12 16:12:57 +0200

    test: add e2e test

---
+++ tests/verbose-output.rs
@@ -0,0 +1,25 @@
+
+use std::process::Command;
+
+use anyhow::{Result, Context, anyhow};
+
+#[test]
+fn test_executable() -> Result<()> {
+    let example_name = "main";
+    
+    let output = Command::new("cargo")
+        .args(["run", "--quiet", "--bin", example_name, "--", "start", "--dry-run"])
+        .output()
+        .with_context(|| anyhow!("running {example_name:?}"))?;
+
+    assert!(output.status.success());
+
+    assert!(output.stdout.len() == 0);
+
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    let expected_stderr = include_str!("verbose-output.out");
+    assert_eq!(stderr, expected_stderr);
+
+    Ok(())
+}
+
