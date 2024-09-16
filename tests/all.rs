9f65f2a10f32af06c919068b7afe0f0a02a9d75a
Christian Jaeger <ch@christianjaeger.ch>
2024-09-16 11:22:55 +0200

    test: replace include with indoc

--- tests/all.rs
+++ tests/all.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
 
 use anyhow::{anyhow, Context, Result};
+use indoc::indoc;
 
 #[test]
 fn verify_verbose_output() -> Result<()> {
@@ -24,7 +25,23 @@ fn verify_verbose_output() -> Result<()> {
     assert!(output.stdout.len() == 0);
 
     let stderr = String::from_utf8_lossy(&output.stderr);
-    let expected_stderr = include_str!("verbose-output.out");
+    let expected_stderr = indoc! {"
+        + ip6tables -t filter -D our-chain -i eth0 -j REJECT
+        + ip6tables -t filter -D our-chain -i eth0 -p tcp --dport 9080 -j RETURN
+        + ip6tables -t filter -D our-chain -i eth0 -p tcp --dport 80 -j RETURN
+        + ip6tables -t filter -D our-chain -i eth0 -p tcp --dport 22 -j RETURN
+        + ip6tables -t filter -D FORWARD -j our-chain
+        + ip6tables -t filter -D INPUT -j our-chain
+        + ip6tables -t filter -F our-chain
+        + ip6tables -t filter -X our-chain
+        + ip6tables -t filter -N our-chain
+        + ip6tables -t filter -I INPUT 1 -j our-chain
+        + ip6tables -t filter -I FORWARD 1 -j our-chain
+        + ip6tables -t filter -A our-chain -i eth0 -p tcp --dport 22 -j RETURN
+        + ip6tables -t filter -A our-chain -i eth0 -p tcp --dport 80 -j RETURN
+        + ip6tables -t filter -A our-chain -i eth0 -p tcp --dport 9080 -j RETURN
+        + ip6tables -t filter -A our-chain -i eth0 -j REJECT
+    "};
     assert_eq!(stderr, expected_stderr);
 
     Ok(())
